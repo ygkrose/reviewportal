cef409fd5f5ecc61a6cc3cb0b4c100470dfa2543
jack <ygkrose@gmail.com>
2016-08-03 21:10:41 +0800

    eatmongo

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,12 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 public partial class _Default : System.Web.UI.Page
 {
+    private MongoClient _client;
+    private IMongoDatabase _database;
+    private IMongoCollection<BsonDocument> _collection;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         try
         {
+            _client = new MongoClient("mongodb://ygkroses:4rfv5tgb@ds033015.mlab.com:33015/acct");
+            _database = _client.GetDatabase("acct");
+            _collection = _database.GetCollection<BsonDocument>("account");
             //Response.Write(ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString());
         }
         catch (Exception err)
