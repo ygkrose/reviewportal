1829d0f525239c8d4539dbe157f9b681e893284f
jack <ygkrose@gmail.com>
2016-08-03 20:56:33 +0800

    test1

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Configuration;
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Response.Write(ConfigurationManager.AppSettings["MONGOLAB_URI"].ToString());
     }
 }
