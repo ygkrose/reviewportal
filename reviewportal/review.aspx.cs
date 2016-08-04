299aa3104dd2fb17c8234455213eb956784ccf79
jack <ygkrose@gmail.com>
2016-08-04 17:51:52 +0800

    mongo test1

--- reviewportal/review.aspx.cs
+++ reviewportal/review.aspx.cs
@@ -5,19 +5,26 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MongoDB.Driver;
+using MongoDB.Bson;
+
 
 namespace reviewportal
 {
     public partial class review : System.Web.UI.Page
     {
+        private MongoClient _client;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            LoadMongoDB();
         }
 
         private void LoadMongoDB()
         {
-
+            
+            _client = new MongoClient("mongodb://ygkroses:4rfv5tgb@ds033015.mlab.com:33015/acct");
+            MongoDatabase _database = _client.GetServer().GetDatabase("acct");
+            
         }
     }
 }
